c786b50ebf2ed1a87fca867c72cb37885e5b7421
yamachu <yamachu.dev@gmail.com>
2023-01-18 16:48:59 +0900

    Add baseline

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  time: string;
+};
+
+export const getData = (): Data => ({
+  time: JSON.stringify(new Date()),
+});
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(getData());
 }
